33381f254ea15a7786c4e01fdd7e563803d61b66
Abhay Raizada <toabhayraizada@gmail.com>
2021-08-25 13:45:42 +0530

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+};
+
+entrypoint!(main);
+
+fn main(
+    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
+    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+) -> ProgramResult {
+    //println!("Hello, world!");
+    Ok(())
+}
